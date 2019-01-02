67fec78e0229ea686de3282313e26314f9b049c5
Andy Clark <ajc97@cantab.net>
2019-01-02 10:10:15 +0000

    Inserting new tasks into the db

--- app/src/main/java/home/westering56/taskbox/AddTaskActivity.java
+++ app/src/main/java/home/westering56/taskbox/AddTaskActivity.java
@@ -2,22 +2,32 @@ package home.westering56.taskbox;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+import home.westering56.taskbox.room.Task;
+import home.westering56.taskbox.viewmodel.TaskViewModel;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class AddTaskActivity extends AppCompatActivity {
 
     private ActionBar actionBar;
+    private TaskViewModel taskViewModel;
+    private TextView editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
+        editText = findViewById(R.id.editText);
         actionBar = getSupportActionBar();
-        actionBar.setTitle("New home.westering56.taskbox.room.Task");
+        actionBar.setTitle("New Task");
         actionBar.setHomeAsUpIndicator(android.R.drawable.ic_menu_close_clear_cancel);
+
+        taskViewModel = ViewModelProviders.of(this).get(TaskViewModel.class);
     }
 
     @Override
@@ -30,7 +40,11 @@ public class AddTaskActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.save_menu_item) {
-            // TODO: Save menu action
+            String summary = editText.getText().toString();
+            if (summary.length() > 0) {
+                taskViewModel.insert(new Task(summary));
+            }
+            finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
