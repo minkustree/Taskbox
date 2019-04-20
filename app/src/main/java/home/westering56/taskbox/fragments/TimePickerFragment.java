e585c3a16da7ba363803670d5194cd542bea46b0
Andy Clark <ajc97@cantab.net>
2019-04-20 12:08:46 +0100

    Fragments must be public. Stupid linter (part 2)

--- app/src/main/java/home/westering56/taskbox/fragments/TimePickerFragment.java
+++ app/src/main/java/home/westering56/taskbox/fragments/TimePickerFragment.java
@@ -13,7 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
 
     private CancellableOnTimeSetListener mOnTimeSetListener;
     private LocalTime mInitialTime;
